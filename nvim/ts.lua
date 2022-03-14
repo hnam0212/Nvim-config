42ed22e251e954e1d95dcc4a86ab5bdfd4310ad6
hnam0212 <hoangnam021200@gmail.com>
2022-03-14 15:31:32 +0700

    first commit

---
+++ nvim/ts.lua
@@ -0,0 +1,32 @@
+require'nvim-treesitter.configs'.setup {
+  rainbow = {
+      enable = true,
+      -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
+      extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+      max_file_lines = nil, -- Do not enable for files with more than n lines, int
+      colors = {"#f54269","#00FF00","#4260f5","#f57242","#f5ec42"}, -- table of hex strings
+      -- termcolors = {} -- table of colour name strings
+    },
+  ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  ignore_install = { "" }, -- List of parsers to ignore installing
+  highlight = {
+    enable = true,              -- false will disable the whole extension
+    disable = { "" },  -- list of language that will be disabled
+    custom_captures = {
+      -- Highlight the @foo.bar capture group with the "Identifier" highlight group.
+      ["foo.bar"] = "Identifier",
+    },
+  },
+  indent = {
+    enable = true
+  },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "gnn",
+      node_incremental = "grn",
+      scome_incremental = "mrc",
+      node_decremental = "grm",
+    },
+  }
+}
